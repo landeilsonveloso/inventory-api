3116d6bc3bcb76aafca046381671d1c3e90eaa36
landeilsonveloso <landeilsonveloso@gmail.com>
2025-05-21 14:37:23 -0300

    ajustes

--- src/config/schemas.js
+++ src/config/schemas.js
@@ -1,11 +1,17 @@
 import Category from "../models/categories.js"
+import Client from "../models/clients.js"
 import database from "./database.js"
+import Product from "../models/products.js"
+import Supplier from "../models/suppliers.js"
 import User from "../models/users.js"
 
 export const syncAll = async () => {
     try {
         await User.sync({alter: true})
         await Category.sync({alter: true})
+        await Supplier.sync({alter: true})
+        await Product.sync({alter: true})
+        await Client.sync({alter: true})
     }
     
     catch (error) {
